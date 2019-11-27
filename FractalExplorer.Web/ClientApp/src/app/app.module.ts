c85ca6305b5d236d4d68cab88dafb7284a5681bd
Chris Elliott <cjelliott@gmail.com>
2019-11-27 13:40:11 +1100

    removed all bootstrap references and default navmenu component

--- FractalExplorer.Web/ClientApp/src/app/app.module.ts
+++ FractalExplorer.Web/ClientApp/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
-import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { HomeComponent } from './components/home/home.component';
 import { ParametersComponent } from './components/parameters/parameters.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
     ParametersComponent,
   ],
